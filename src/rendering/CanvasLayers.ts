30a59e726524b2b102d1a86c5d51573d62b5ac75
gongahkia <angryapplegravy@gmail.com>
2025-06-17 09:37:40 +0800

    feat(app): fixed

--- src/rendering/CanvasLayers.ts
+++ src/rendering/CanvasLayers.ts
@@ -1,44 +1,47 @@
+import { mat4 } from 'gl-matrix';
+
+export function initializeWebGL2(canvas: HTMLCanvasElement): WebGL2RenderingContext {
+  const gl = canvas.getContext('webgl2', {
+    alpha: false,
+    antialias: false,
+    premultipliedAlpha: false
+  });
+
+  if (!gl) throw new Error('WebGL2 not supported');
+  
+  gl.enable(gl.BLEND);
+  gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+  gl.pixelStorei(gl.UNPACK_ALIGNMENT, 1);
+
+  return gl;
+}
+
 export class CanvasLayers {
-  private gl: WebGL2RenderingContext;
+  public readonly gl: WebGL2RenderingContext;
   private canvas: HTMLCanvasElement;
-  private layers: Map<string, WebGL2RenderingContext> = new Map();
   private projectionMatrix = new Float32Array(16);
 
   constructor(canvasId: string = 'gameCanvas') {
     this.canvas = document.getElementById(canvasId) as HTMLCanvasElement;
-    this.gl = this.initializeWebGL2();
+    this.gl = initializeWebGL2(this.canvas); // Use standalone function
     this.createOrthoMatrix();
-    this.handleResize();
-    window.addEventListener('resize', this.handleResize.bind(this));
+    this.initResizeHandler();
   }
 
-  private initializeWebGL2(): WebGL2RenderingContext {
-    const gl = this.canvas.getContext('webgl2', {
-      alpha: false,
-      antialias: false,
-      premultipliedAlpha: false
-    });
-
-    if (!gl) throw new Error('WebGL2 not supported');
-    
-    gl.enable(gl.BLEND);
-    gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
-    gl.pixelStorei(gl.UNPACK_ALIGNMENT, 1);
-
-    return gl;
+  private initResizeHandler(): void {
+    const resizeObserver = new ResizeObserver(() => this.handleResize());
+    resizeObserver.observe(this.canvas);
   }
 
   private createOrthoMatrix(): void {
-    const near = -1000;
-    const far = 1000;
     mat4.ortho(
       this.projectionMatrix,
       0,
       this.canvas.width,
       this.canvas.height,
       0,
-      near,
-      far
+      -1000,
+      1000
     );
   }
 
@@ -54,14 +57,10 @@ export class CanvasLayers {
     }
   }
 
-  beginFrame(): void {
+  public beginFrame(): void {
     this.gl.viewport(0, 0, this.canvas.width, this.canvas.height);
     this.gl.clearColor(0.1, 0.1, 0.1, 1);
     this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
-  }
-
-  get context(): WebGL2RenderingContext {
-    return this.gl;
   }
 
   get projection(): Float32Array {
