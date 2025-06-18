62c10cc8cbb775f6567fe3dce3fe59017da095a7
Gabriel Ong Zhe Mian <angryapplegravy@gmail.com>
2025-06-18 15:40:44 +0800

    feat(app): noted modules

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import GameStateProvider from './store/gameState';
 import { AIWorkerProvider } from './AIWorkerContext';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <AIWorkerProvider>
-      <App />
+    <App />
   </AIWorkerProvider>
 );
