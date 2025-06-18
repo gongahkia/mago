b5f6bf40879909d2f29a7f78b7bee2d62ff4699c
Gabriel Ong Zhe Mian <angryapplegravy@gmail.com>
2025-06-18 12:31:22 +0800

    feat(app): shit passes u by real quick

--- src/game/dungeon/algorithms/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './CellularAutomata';
-export * from './BSPTree';
--- src/utils/worker-loader.ts
+++
@@ -1,3 +0,0 @@
-export function createWorker<T>(url: URL | string): T {
-  return new Worker(url, { type: 'module' }) as unknown as T;
-}
