1ad41e1df77f8072e0ef905b3b34330acc1ca289
gongahkia <angryapplegravy@gmail.com>
2025-06-23 14:07:10 +0800

    feat(app): compassion begets compassion

---
+++ mago-app-v2/frontend/webpack.config.js
@@ -0,0 +1,52 @@
+/**
+ * Webpack configuration for development and production
+ * Bundles JavaScript modules for browser execution
+ */
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    clean: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      favicon: './public/favicon.ico'
+    })
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 3000,
+    hot: true,
+    proxy: {
+      '/game': 'http://localhost:8000',
+      '/llm': 'http://localhost:8000'
+    }
+  },
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000
+  }
+};
